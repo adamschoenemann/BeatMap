a6cc374761286c569d48a1f5c96d396088e9a00d
Thorbjørn Wolf <thorbjorn.wolf@gmail.com>
2016-01-16 15:18:56 +0100

    ListView with strings works

--- app/src/main/java/mta/beatmap/app/MainActivity.java
+++ app/src/main/java/mta/beatmap/app/MainActivity.java
@@ -4,6 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +18,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView lv = (ListView) findViewById(R.id.listView);
+        ArrayList<String> myStringArray1 = new ArrayList<String>();
+        myStringArray1.add("something");
+        Collections.addAll(myStringArray1, new String[]{"Android", "iPhone", "WindowsMobile",
+                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+                "Linux", "OS/2"});
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, myStringArray1);
+        lv.setAdapter(adapter);
     }
 
     @Override
