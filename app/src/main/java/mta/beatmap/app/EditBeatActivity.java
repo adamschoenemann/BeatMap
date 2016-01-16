41ed4c1da62a96461859876c23c8eae8762b2bc0
Mick Neupart <mickneupart@gmail.com>
2016-01-16 16:16:55 +0100

    Add EditBeatActivity

---
+++ app/src/main/java/mta/beatmap/app/EditBeatActivity.java
@@ -0,0 +1,13 @@
+package mta.beatmap.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditBeatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_beat);
+    }
+}
