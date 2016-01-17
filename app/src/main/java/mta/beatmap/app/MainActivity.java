c0623f5826e9b799a8e7504fd98baa50d6ea1785
Thorbjørn Wolf <thorbjorn.wolf@gmail.com>
2016-01-17 16:24:04 +0100

    It works! ish

--- app/src/main/java/mta/beatmap/app/MainActivity.java
+++ app/src/main/java/mta/beatmap/app/MainActivity.java
@@ -31,9 +31,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        testDB();
+//        testDB();
 
-        loadTrackListView();
+//        loadTrackListView();
     }
 
     private void loadTrackListView(){
